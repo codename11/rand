cd1de4225122a9e7ba52fe24dfc2d52e93d000ad
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-10-22 09:31:40 +0800

    Tidy up indentation

--- rand.php
+++ rand.php
@@ -1,29 +1,27 @@
-<?php 
+<?php
+	$br1=$_POST['br1'];
+	$br2=$_POST['br2'];
 
-$br1=$_POST['br1'];
-$br2=$_POST['br2'];
-
-//echo$_POST['br1'];
-
+	//echo$_POST['br1'];
 
 	function gen($br1,$br2){
 
-	$br1=(int)$br1;
-	$br2=(int)$br2;
-	
-    $Q=rand($br1,$br2);
-	
-	return $Q;
-	
-}
-
-if(isset($_POST['br1']) && isset($_POST['br2'])){
-	
-	if($_POST['br1']==0 && $_POST['br2']==0){
-		$_POST['br1']=0;
-		$_POST['br2']=0;
+		$br1=(int)$br1;
+		$br2=(int)$br2;
+		
+	    $Q=rand($br1,$br2);
+		
+		return $Q;
+		
 	}
-	
-	echo "<p class='text-info'>"."This is random generated number from ".$_POST["br1"]. " to ".$_POST["br2"].": ".gen($_POST['br1'],$_POST["br2"])."<br>"."</p>";
-}
+
+	if(isset($_POST['br1']) && isset($_POST['br2'])) {
+		
+		if($_POST['br1']==0 && $_POST['br2']==0){
+			$_POST['br1']=0;
+			$_POST['br2']=0;
+		}
+		
+		echo "<p class='text-info'>"."This is random generated number from ".$_POST["br1"]. " to ".$_POST["br2"].": ".gen($_POST['br1'],$_POST["br2"])."<br>"."</p>";
+	}
 ?>
